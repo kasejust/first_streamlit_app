10f35817e4b5270a02755bf1d1c881a96767f9f0
kasejust <151584042+kasejust@users.noreply.github.com>
2023-11-20 17:22:27 -0700

    Update test3.py

--- test3.py
+++ test3.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Healthy Bluberry and Omega 3 Oatmeal')
+streamlit.text('Chicken Fried Steak eggs and gravy')
+streamlit.text('kale spinach avacado toast')
