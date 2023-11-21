19431b2c7a6356250759149d50502e2e847cc5c8
kasejust <151584042+kasejust@users.noreply.github.com>
2023-11-21 09:16:51 -0700

    Update test3.py

--- test3.py
+++ test3.py
@@ -7,3 +7,6 @@ streamlit.text('Chicken Fried Steak eggs and gravy')
 streamlit.text('kale spinach avacado toast')
 streamlit.text('🥣 🥗 🐔 🥑🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
