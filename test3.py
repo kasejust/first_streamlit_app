aab2315da4feedb5f831af65281dc68244603b67
kasejust <151584042+kasejust@users.noreply.github.com>
2023-11-22 10:15:42 -0700

    Update test3.py

--- test3.py
+++ test3.py
@@ -77,6 +77,7 @@ try:
   else:
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+     my_cnx.close()
     streamlit.text(back_from_function)
 except URLError as e:
  streamlit.error()
