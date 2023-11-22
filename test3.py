c82e47eed8e7ba87f3d85af46be4ad66d88f9157
kasejust <151584042+kasejust@users.noreply.github.com>
2023-11-22 08:50:24 -0700

    Update test3.py

--- test3.py
+++ test3.py
@@ -9,6 +9,18 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 
+add_my_fruit = streamlit.text_input('What fruit would you like add?')
+streamlit.write('Thanks for adding ', add_my_fruit)
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+#streamlit.text(fruityvice_response.json())
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+#streamlit.dataframe(fruityvice_normalized)
+
+
+
+
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Healthy Bluberry and Omega 3 Oatmeal')
